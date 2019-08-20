ef7cd50bec9d732665de09768b71adffa2c75d8c
Chris443 <Christiankati@gmx.net>
2019-08-20 20:02:43 +0200

    rumgespielt xoxo

--- GeKo/Applications/KatiApplication/K_App.cpp
+++ GeKo/Applications/KatiApplication/K_App.cpp
@@ -28,19 +28,20 @@ void K_App::run() {
 }
 
 void K_App::init() {
-
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	shader.create("./Applications/KatiApplication/Shader/basic.vert",
 		"./Applications/KatiApplication/Shader/basic.frag");
 
 	float vertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+	//positions			//colors
+	 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,// top right
+	 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,// bottom right
+	-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,// bottom left
+	-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,// top left 
 	};
 	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
+		0, 1, 2,     // second triangle
+		3, 0, 2   // first triangle
 	};
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -48,8 +49,10 @@ void K_App::init() {
 	vbo.create(vertices, sizeof(vertices));
 	ebo.create(indices, sizeof(indices));
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float) ));
+	glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
 }
 
@@ -58,7 +61,6 @@ void K_App::render() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	shader.bind();
-
 
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -74,7 +76,7 @@ void K_App::update(float deltaTime) {
 
 	gkm::mat4 transformMat;
 
-	transformMat.translate(gkm::vec3(gkm::d_cos(v),0,0));	
+	//transformMat.translate(gkm::vec3(gkm::d_cos(v),0,0));	
 	transformMat.euler_rotate( gkm::vec3(0,v,v) );
 	//transformMat.euler_rotate(gkm::vec3(0, v, 0));
 	
--- GeKo/Applications/KatiApplication/Shader/basic.vert
+++ GeKo/Applications/KatiApplication/Shader/basic.vert
@@ -1,10 +1,13 @@
 #version 330 core
-layout (location = 0)
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
 
-in vec3 aPos;
 uniform mat4 mat;
+
+out vec3 color;
 
 void main()
 {
 	gl_Position = mat * vec4(aPos , 1.0);
+	color = aColor;
 }
